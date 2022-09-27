0bd905569175703d971aec4ac6a161f3888065e5
osvilla27 <oscaravg27@gmail.com>
2022-09-27 14:45:32 -0500

    Start app 10

--- manager/settings.py
+++ manager/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
-    'drf_yasg',
+    # 'drf_yasg',
 
     'accounts',
     'store',
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- manager/urls.py
+++ manager/urls.py
@@ -2,23 +2,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 from store.api.router import router_store
 from customer.api.router import router_customer
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Manager - API",
-        default_version='v1',
-        description="API DOC de Manager",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="oscaravg27@gmail.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    # permission_classes=[permissions.AllowAny],
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Manager - API",
+#         default_version='v1',
+#         description="API DOC de Manager",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="oscaravg27@gmail.com"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     # permission_classes=[permissions.AllowAny],
+# )
 
 urlpatterns = [
     path('manager/', admin.site.urls),
@@ -27,10 +27,10 @@ urlpatterns = [
     path('api/', include(router_store.urls)),
     path('api/', include(router_customer.urls)),
     
-    path('docs/', schema_view.with_ui('swagger',
-         cache_timeout=0), name='schema-swagger-ui'),
-    path('redocs/', schema_view.with_ui('redoc',
-         cache_timeout=0), name='schema-redoc'),
+    # path('docs/', schema_view.with_ui('swagger',
+    #      cache_timeout=0), name='schema-swagger-ui'),
+    # path('redocs/', schema_view.with_ui('redoc',
+    #      cache_timeout=0), name='schema-redoc'),
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
